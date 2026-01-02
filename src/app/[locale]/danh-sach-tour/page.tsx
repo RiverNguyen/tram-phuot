b05c58c5b38ae80b5f8b6bcf302e45730edffc31
sylph03 <sylph203@gmail.com>
2026-01-02 10:40:57 +0700

    feat: add ui page list tour

--- src/app/[locale]/danh-sach-tour/page.tsx
+++ src/app/[locale]/danh-sach-tour/page.tsx
@@ -1,7 +1,6 @@
-import Banner from './sections/Banner'
-import SpecialOffers from './sections/SpecialOffers'
-import OngoingPromotions from './sections/OngoingPromotions'
 import ICElipse from '@/components/icon/ICElipse'
+import Banner from './sections/Banner'
+import TourList from './sections/TourList'
 
 export default function page() {
   return (
@@ -10,13 +9,9 @@ export default function page() {
       <Banner />
 
       {/* Main content */}
-      <div className='xsm:px-[1rem] xsm:py-[2.5rem] xsm:gap-[2.5rem] relative w-full max-w-[87.5rem] mx-auto flex flex-col items-center gap-[5.625rem] py-[5rem]'>
-        {/* Special offer just for you! */}
-        <SpecialOffers />
-
-        {/* ongoing promotion */}
-        <OngoingPromotions />
-
+      <div className='xsm:px-[1rem] xsm:py-[2.5rem] xsm:gap-[2.5rem] relative w-full h-full max-w-[87.5rem] mx-auto flex flex-col items-center gap-[3.75rem] pt-[5rem] pb-[3.75rem]'>
+        {/* Tour list */}
+        <TourList />
         {/* pagination */}
         <div className='flex items-center gap-[0.625rem]'>
           <div className='flex items-center justify-center size-[2rem] rounded-full bg-[rgba(4,52,36,0.10)] text-[#043424] font-montserrat text-[0.875rem] leading-[1.3125rem] hover:bg-[linear-gradient(230deg,#03328C_5.76%,#00804D_100.15%)] hover:text-white transition-all duration-300 cursor-pointer'>
---
+++ src/components/icon/ICChevronright.tsx
@@ -0,0 +1,36 @@
+import { SVGProps } from 'react'
+
+export default function ICChevronright(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='9'
+      height='12'
+      viewBox='0 0 9 12'
+      fill='none'
+      {...props}
+    >
+      <g clipPath='url(#clip0_1948_5724)'>
+        <path
+          d='M0.858398 1.48438L6.95385 6.11692L0.858398 10.4026'
+          stroke='#DED8C5'
+          strokeWidth='2.04545'
+        />
+        <path
+          d='M0.858398 1.48438L6.95385 6.11692L0.858398 10.4026'
+          stroke='white'
+          strokeWidth='2.04545'
+        />
+      </g>
+      <defs>
+        <clipPath id='clip0_1948_5724'>
+          <rect
+            width='9'
+            height='12'
+            fill='white'
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
