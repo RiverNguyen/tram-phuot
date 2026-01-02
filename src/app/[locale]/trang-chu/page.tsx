39d9714bed36a69b9e03dd8fad055ab36e15d0e9
sylph03 <sylph203@gmail.com>
2026-01-02 22:30:44 +0700

    update ui section 4 the explorers homepage

--- src/app/[locale]/trang-chu/page.tsx
+++ src/app/[locale]/trang-chu/page.tsx
@@ -2,10 +2,8 @@ import TheExplorers from './sections/TheExplorers'
 
 export default function Home() {
   return (
-    <main className='relative w-full h-full bg-[#FDF4ED]'>
-      <div className='w-full h-[50.83rem] bg-[#FDF4ED]'></div>
+    <main className='relative w-full h-full bg-[#FDF4ED] overflow-hidden'>
       <TheExplorers />
-      <div className='w-full h-[50.83rem] bg-[#FDF4ED]'></div>
     </main>
   )
 }
--- src/app/[locale]/trang-chu/sections/TheExplorers.tsx
+++ src/app/[locale]/trang-chu/sections/TheExplorers.tsx
@@ -6,6 +6,9 @@ import Link from 'next/link'
 import ICBgTitle from '@/components/icon/ICBgTitle'
 import { gsap } from 'gsap'
 import { useGSAP } from '@gsap/react'
+import { CustomEase } from 'gsap/dist/CustomEase'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
 
 const SOCIALS = [
   {
@@ -44,6 +47,8 @@ export default function TheExplorers() {
   const containerRef = useRef<HTMLDivElement>(null)
   useGSAP(
     () => {
+      CustomEase.create('explorersEase', '0.51,-0.01,0.12,1')
+
       const mm = gsap.matchMedia()
 
       mm.add(
@@ -52,131 +57,80 @@ export default function TheExplorers() {
           isDesktop: '(min-width: 640px)',
         },
         (ctx) => {
-          const { isMobile } = ctx.conditions!
+          if (ctx.conditions?.isMobile) return
 
           const tl = gsap.timeline({
             defaults: {
-              duration: 2,
-              ease: 'power3.out',
+              duration: 1.6,
+              ease: 'explorersEase',
+            },
+            scrollTrigger: {
+              trigger: containerRef.current,
+              start: 'top 50%',
+              toggleActions: 'play none none none',
+              once: true,
             },
           })
 
           // Background
-          tl.fromTo('.bg-animation', { scale: 1.5 }, { scale: 1 })
+          tl.fromTo('.bg-animation', { scale: 1.559 }, { scale: 1 })
 
           // Bricks
           tl.fromTo(
             ['.brick-1', '.brick-3', '.turle-tower'],
-            {
-              y: (i) => [100, 140, 100][i],
-            },
-            { y: 0 },
-            '<', // chạy song song
-          )
-
-          // Halo
-          tl.fromTo('.halo', { opacity: 1 }, { opacity: 0.6 }, '<')
-
-          // Towels
-          tl.fromTo(
-            '.towel-left',
-            {
-              y: 100,
-              rotate: -12.03,
-            },
-            {
-              y: 0,
-              rotate: 0,
-            },
+            { y: (i) => ['6.3418rem', '8.8418rem', '10.1504rem'][i] },
+            { y: '0rem' },
             '<',
           )
+
+          // Fade elements
+          tl.fromTo(['.rim-100', '.flower', '.stamp-1'], { opacity: 0 }, { opacity: 1 }, '<')
+
+          tl.fromTo('.rim-50', { opacity: 0 }, { opacity: 0.5 }, '<')
+          tl.fromTo('.halo', { opacity: 1 }, { opacity: 0.6 }, '<')
+
+          // Vertical move (no rotate)
+          tl.fromTo(
+            ['.street', '.image-dynamic-2', '.image-dynamic-4', '.content'],
+            {
+              y: (i) => ['9.4946rem', '7.063rem', '5.9111rem', '5.1226rem'][i],
+            },
+            { y: '0rem' },
+            '<',
+          )
+
+          // Towels
+          tl.fromTo('.towel-left', { y: '9.4946rem', rotate: -7.32 }, { y: '0rem', rotate: 0 }, '<')
 
           tl.fromTo(
             '.towel-right',
-            {
-              y: 100,
-              rotate: 25.45,
-            },
-            {
-              y: 0,
-              rotate: 0,
-            },
+            { y: '12.9054rem', rotate: 19.05 },
+            { y: '0rem', rotate: 0 },
             '<',
           )
 
-          // street
-          tl.fromTo(
-            '.street',
-            {
-              y: 100,
-            },
-            {
-              y: 0,
-            },
-            '<',
-          )
-
-          // person
-          tl.fromTo(
-            '.person',
-            {
-              y: 50,
-            },
-            {
-              y: 0,
-            },
-            '<',
-          )
-
-          // image dynamic 1
+          // Dynamic images (rotate)
           tl.fromTo(
             '.image-dynamic-1',
-            {
-              y: 100,
-              rotate: -12.03,
-            },
-            {
-              y: 0,
-              rotate: -5.681,
-            },
+            { y: '6.5308rem', rotate: -18.44 },
+            { y: '0rem', rotate: -5.68 },
             '<',
           )
 
-          // image dynamic 2
-          tl.fromTo(
-            '.image-dynamic-2',
-            {
-              y: 100,
-            },
-            {
-              y: 0,
-            },
-            '<',
-          )
-
-          // image dynamic 3
           tl.fromTo(
             '.image-dynamic-3',
-            {
-              y: 100,
-              rotate: 39.058,
-            },
-            {
-              y: 0,
-              rotate: 19.529,
-            },
+            { y: '7.2029rem', rotate: 32.36 },
+            { y: '0rem', rotate: 19.53 },
             '<',
           )
 
-          // image dynamic 4
+          // Stamp + Title
+          tl.fromTo('.stamp-2', { y: '7.9126rem' }, { y: '0rem' }, '<')
+
           tl.fromTo(
-            '.image-dynamic-4',
-            {
-              y: 100,
-            },
-            {
-              y: 0,
-            },
+            '.title',
+            { y: '16.4952rem', opacity: 0, rotate: -14.67 },
+            { y: '0rem', opacity: 1, rotate: 0 },
             '<',
           )
         },
@@ -190,17 +144,17 @@ export default function TheExplorers() {
   return (
     <div
       ref={containerRef}
-      className='relative w-full h-[50.83rem] max-w-[100rem] mx-auto overflow-hidden'
+      className='xsm:px-[1rem] xsm:h-[50.9375rem] relative w-full h-[50.83rem] max-w-[100rem] mx-auto'
     >
       {/* Background */}
-      <ICBgExplorers className='bg-animation absolute top-[7.88rem] left-1/2 -translate-x-1/2 w-[96.25rem] h-[35.75rem]' />
+      <ICBgExplorers className=' xsm:top-[2rem] xsm:rotate-[90deg] xsm:w-full xsm:h-[43.5625rem] bg-animation absolute top-[7.88rem] left-1/2 -translate-x-1/2 w-[96.25rem] h-[35.75rem]' />
       {/* Brick 1 */}
       <Image
         src='/home/brick.webp'
         alt='Brick 1'
         width={120}
         height={120}
-        className='brick-1 z-1 absolute top-[13.4rem] left-[59.9259rem] w-[2.49169rem] h-auto'
+        className='brick-1 z-1 absolute top-[13.4rem] left-[59.5509rem] w-[2.49169rem] h-auto'
       />
       {/* Brick 2 */}
       <Image
@@ -208,7 +162,7 @@ export default function TheExplorers() {
         alt='Brick 2'
         width={120}
         height={120}
-        className='z-1 absolute top-[31.72rem] left-[93.6886rem] w-[2.49169rem] h-auto'
+        className='z-1 absolute top-[31.72rem] left-[93.3136rem] w-[2.49169rem] h-auto'
       />
       {/* Brick 3 */}
       <Image
@@ -216,7 +170,7 @@ export default function TheExplorers() {
         alt='Brick 2'
         width={120}
         height={120}
-        className='brick-3 z-1 absolute top-[15.89rem] left-[57.4342rem] w-[2.49169rem] h-auto'
+        className='brick-3 z-1 absolute top-[15.89rem] left-[57.0592rem] w-[2.49169rem] h-auto'
       />
       {/* Brick 4 */}
       <Image
@@ -224,10 +178,10 @@ export default function TheExplorers() {
         alt='Brick 2'
         width={120}
         height={120}
-        className='z-1 absolute top-[34.21rem] left-[91.1969rem] w-[2.49169rem] h-auto'
+        className='z-1 absolute top-[34.21rem] left-[90.8219rem] w-[2.49169rem] h-auto'
       />
 
-      <div className='relative top-[8.51rem] left-1/2 -translate-x-1/2 w-[96.25rem] h-[35.125rem]'>
+      <div className='z-1 relative top-[8.51rem] left-1/2 -translate-x-1/2 w-[96.25rem] h-[35.125rem]'>
         {/* halo */}
         <div className='halo absolute top-[2.3447rem] left-[56.0557rem] w-[18.32988rem] h-[18.52913rem] bg-[linear-gradient(154deg,rgba(255,183,21,0.60)_19.82%,rgba(240,76,5,0.60)_91.61%)] blur-[55.2px] opacity-[0.6]'></div>
         {/* rim 100 */}
@@ -236,7 +190,7 @@ export default function TheExplorers() {
           alt='Rim'
           width={1023}
           height={374}
-          className='absolute top-[28.084rem] left-[13.3591rem] w-[21.30675rem] h-auto object-cover'
+          className='rim-100 absolute top-[28.084rem] left-[13.3591rem] w-[21.30675rem] h-auto object-cover'
         />
         {/* rim 50 */}
         <Image
@@ -244,7 +198,7 @@ export default function TheExplorers() {
           alt='Rim'
           width={1023}
           height={374}
-          className='absolute top-[1.979rem] left-[75.7878rem] w-[21.30675rem] h-auto object-cover opacity-50'
+          className='rim-50 absolute top-[1.979rem] left-[75.7878rem] w-[21.30675rem] h-auto object-cover opacity-50'
         />
         {/* flower */}
         <Image
@@ -252,7 +206,7 @@ export default function TheExplorers() {
           alt='Flower'
           width={624}
           height={513}
-          className='absolute top-[16.2202rem] left-[53.0031rem] w-[13rem] h-auto object-cover'
+          className='flower absolute top-[16.2202rem] left-[53.0031rem] w-[13rem] h-auto object-cover'
         />
         {/* towel left */}
         <Image
@@ -315,7 +269,7 @@ export default function TheExplorers() {
           />
         </div>
         {/* image dynamic 3 */}
-        <div className='image-dynamic-3 z-2 absolute top-[14.7rem] left-[77.2rem] w-[12.68819rem] h-[18.03244rem] rotate-[19.529deg] p-[0.35rem] overflow-hidden bg-white shadow-[-107.323px_215.505px_66.97px_0_rgba(0,0,0,0),-68.687px_137.373px_61.818px_0_rgba(0,0,0,0.01),-38.636px_77.273px_52.374px_0_rgba(0,0,0,0.05),-17.172px_34.343px_38.636px_0_rgba(0,0,0,0.09),-4.293px_8.586px_21.465px_0_rgba(0,0,0,0.10)]'>
+        <div className='image-dynamic-3 z-2 absolute top-[14.7rem] left-[77.2rem] w-[12.68819rem] h-[18.03244rem] rotate-[19.529deg] p-[0.35rem] overflow-hidden bg-white shadow-[-100.311px_201.425px_62.594px_0_rgba(0,0,0,0),-64.199px_128.399px_57.779px_0_rgba(0,0,0,0.01),-36.112px_72.224px_48.952px_0_rgba(0,0,0,0.05),-16.05px_32.1px_36.112px_0_rgba(0,0,0,0.09),-4.012px_8.025px_20.062px_0_rgba(0,0,0,0.1)]'>
           <Image
             src='/uu-dai/card.webp'
             alt='Image dynamic 3'
@@ -325,7 +279,7 @@ export default function TheExplorers() {
           />
         </div>
         {/* image dynamic 4 */}
-        <div className='image-dynamic-4 z-2 absolute top-[22.8639rem] left-[55.9025rem] w-[14.9538rem] h-[12.6341rem] rotate-[11.21deg] p-[0.35rem] overflow-hidden bg-white shadow-[-107.323px_215.505px_66.97px_0_rgba(0,0,0,0),-68.687px_137.373px_61.818px_0_rgba(0,0,0,0.01),-38.636px_77.273px_52.374px_0_rgba(0,0,0,0.05),-17.172px_34.343px_38.636px_0_rgba(0,0,0,0.09),-4.293px_8.586px_21.465px_0_rgba(0,0,0,0.10)]'>
+        <div className='image-dynamic-4 z-2 absolute top-[22.8639rem] left-[55.9025rem] w-[14.9538rem] h-[12.6341rem] rotate-[11.21deg] p-[0.35rem] overflow-hidden bg-white shadow-[-109.563px_220.003px_68.367px_0_rgba(0,0,0,0),-70.12px_140.241px_63.108px_0_rgba(0,0,0,0.01),-39.443px_78.885px_53.467px_0_rgba(0,0,0,0.05),-17.53px_35.06px_39.443px_0_rgba(0,0,0,0.09),-4.383px_8.765px_21.913px_0_rgba(0,0,0,0.1)]'>
           <Image
             src='/uu-dai/card.webp'
             alt='Image dynamic 4'
@@ -340,10 +294,10 @@ export default function TheExplorers() {
           alt='Stamp 1'
           width={431}
           height={411}
-          className='z-3 absolute top-[15.9298rem] left-[52rem] w-[7.20056rem] h-auto '
+          className='stamp-1 z-3 absolute top-[15.9298rem] left-[52rem] w-[7.20056rem] h-auto '
         />
         {/* content */}
-        <div className='absolute top-[5.4077rem] left-[16.9103rem] flex w-[32.0625rem] flex-col justify-center gap-[1.4375rem]'>
+        <div className='content absolute top-[5.4077rem] left-[16.9103rem] flex w-[32.0625rem] flex-col justify-center gap-[1.4375rem]'>
           <p className='text-white text-[1.125rem] font-montserrat leading-[200%] tracking-[-2%] whitespace-pre-line underline decoration-[0.0625rem] decoration-[rgba(165,165,165,0.50)] underline-offset-[0.5rem] decoration-dashed decoration before:content-[""] before:pl-[3.875rem]'>
             {TEXTAREA}
           </p>
@@ -366,7 +320,7 @@ export default function TheExplorers() {
           </div>
         </div>
         {/* title */}
-        <div className='absolute top-[-7.1431rem] left-[4.3235rem] w-[20.50531rem] h-[14.52263rem]'>
+        <div className='title absolute top-[-7.1431rem] left-[4.3235rem] w-[20.50531rem] h-[14.52263rem]'>
           <ICBgTitle className='absolute top-[1.864rem] left-0 w-[20.50531rem] h-[12.65856rem]' />
           <div className='absolute top-[1.15rem] left-[3.3042rem] rotate-[-3.64deg]'>
             {/* stroke */}
@@ -384,14 +338,13 @@ export default function TheExplorers() {
           </h2>
         </div>
       </div>
-      <div></div>
       {/* stamp 2 */}
       <Image
         src='/home/stamp-2.webp'
         alt='Stamp 1'
         width={525}
         height={525}
-        className='z-3 absolute top-[13.32rem] left-[8.4106rem] w-[7.741rem] h-auto '
+        className='stamp-2 z-3 absolute top-[13.32rem] left-[8.0356rem] w-[7.741rem] h-auto '
       />
     </div>
   )
